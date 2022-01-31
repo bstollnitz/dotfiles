31978bb916f58c339cdc9487f5b1cd0ab1701190
Beatriz Stollnitz <bstollnitz@microsoft.com>
2022-01-31 00:56:19 +0000

    A little test.

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-script_name=$0
-dotfiles_dir=$(dirname "$0")
-rm -rf ~/.zshrc
-rm -rf ~/.dircolors
-ln -s $dotfiles_dir/.zshrc ~/.zshrc
-ln -s $dotfiles_dir/.dircolors ~/.dircolors
+pwd > ~/whereami.txt
+
+# script_name=$0
+# dotfiles_dir=$(dirname "$0")
+# rm -rf ~/.zshrc
+# rm -rf ~/.dircolors
+# ln -s $dotfiles_dir/.zshrc ~/.zshrc
+# ln -s $dotfiles_dir/.dircolors ~/.dircolors
 
