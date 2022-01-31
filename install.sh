ddfb4309e329f64fc5b3f5d0fc4046ff020295fc
Beatriz Stollnitz <bstollnitz@microsoft.com>
2022-01-31 04:55:20 +0000

    Add symbolic link for spaceship theme.

--- install.sh
+++ install.sh
@@ -22,3 +22,4 @@ conda init zsh
 
 apt-get install powerline fonts-powerline
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
