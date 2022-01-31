2d38022de6ee18453e0cb1e7fefd5fd0680517a8
Beatriz Stollnitz <bstollnitz@microsoft.com>
2022-01-31 01:37:28 +0000

    One more try.

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Get the directory in which this script lives.
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+echo SCRIPT_DIR=$SCRIPT_DIR
+
 echo Removing old files.
-rm -rf $HOME/.zshrc
-rm -rf $HOME/.dircolors
+rm -rf "$HOME/.zshrc"
+rm -rf "$HOME/.dircolors"
 
 echo Linking new dotfiles.
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.zshrc $HOME/.zshrc
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.dircolors $HOME/.dircolors
+ln -s "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
+ln -s "$SCRIPT_DIR/.dircolors" "$HOME/.dircolors"
