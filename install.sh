b3eb0da4eb7bccbfd95940e71b9b669c758adaa0
Beatriz Stollnitz <bstollnitz@microsoft.com>
2022-01-31 00:33:27 +0000

    Added explicit dotfiles dir.

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
+dotfiles_dir = "/workspaces/.codespaces/.persistedshare/dotfiles"
 rm -rf ~/.zshrc
 rm -rf ~/.dircolors
-
-dotfiles_dir = $(pwd)
 ln -s $dotfiles_dir/.zshrc ~/.zshrc
 ln -s $dotfiles_dir/.dircolors ~/.dircolors
 
-# cat .zshrc > ~/.zshrc
-# cat .dircolors > ~/.dircolors
