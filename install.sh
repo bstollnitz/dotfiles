fff4a33e8eb0c173da90da36fed4d59c06a227d6
Beatriz Stollnitz <bstollnitz@microsoft.com>
2022-01-31 05:03:04 +0000

    Another tweak.

--- install.sh
+++ install.sh
@@ -22,4 +22,4 @@ conda init zsh
 
 # apt-get install powerline fonts-powerline
 sudo git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
