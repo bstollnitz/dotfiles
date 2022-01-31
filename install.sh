a66bfc6207cdd13ae6dd2671d70e875167146ea6
Beatriz Stollnitz <bstollnitz@microsoft.com>
2022-01-31 01:16:19 +0000

    Another attempt to do symlinking.

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-pwd > ~/whereami.txt
-
-# script_name=$0
-# dotfiles_dir=$(dirname "$0")
-# rm -rf ~/.zshrc
-# rm -rf ~/.dircolors
-# ln -s $dotfiles_dir/.zshrc ~/.zshrc
-# ln -s $dotfiles_dir/.dircolors ~/.dircolors
+rm -rf ~/.zshrc
+rm -rf ~/.dircolors
+ln -s .zshrc ~/.zshrc
+ln -s .dircolors ~/.dircolors
 
