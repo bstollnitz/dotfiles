cbc8b9e4854c8a2ea9411c9b30fcc15c624a8bde
Beatriz Stollnitz <bstollnitz@microsoft.com>
2022-01-31 02:17:15 +0000

    First attempt to initialize conda.

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# Get the directory in which this script lives.
-script_dir=$(dirname "$(readlink -f "$0")")
 
-# Get a list of all files in this directory that start with a dot.
-files=$(find -maxdepth 1 -type f -name ".*")
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
 
-# Create a symbolic link to each file in the home directory.
-for file in $files; do
-    name=$(basename $file)
-    echo "Creating symlink to $name in home directory."
-    rm -rf ~/$name
-    ln -s $script_dir/$name ~/$name
-done
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+}
+
+create_symlinks
+
+
+conda init zsh
